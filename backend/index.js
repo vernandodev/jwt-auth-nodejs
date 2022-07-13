a63624cdad5c1f2c0c0139b16bae07995233fb81
Richovrnndo <Richovrnando@gmail.com>
2022-07-13 09:30:54 +0700

    update depedencies

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,13 @@
 const express = require('express');
 
 const app = express();
+const PORT = 5000;
 
 app.use(express.json());
+
+app.use('/auth', require('./routes/auth'));
+app.use('/posts', require('./routes/posts'));
+
+app.listen(PORT, () => {
+  console.log('listening on port 5000');
+});
